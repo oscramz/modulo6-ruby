5b69b6c0513781eac3ffdba7f494417c71d16a62
oscramz <oscram@gmail.com>
2021-09-01 23:20:47 -0400

    ejercicio pares e impares

--- ejercicio1/ejercicios_for.rb
+++ ejercicio1/ejercicios_for.rb
@@ -12,20 +12,41 @@
 
 #Ejercicio 1 Pares e Impares
 
-dato = 0
-puts "Ingresa un numero y te genero la secuencia par o impar"
-dato = gets.chomp.to_i
-num = 0
+# dato = 0
+# puts "Ingresa un numero y te genero la secuencia par o impar"
+# dato = gets.chomp.to_i
+# num = 0
 
-for i in 0..dato
-    if i % 2 == 0
-        print " #{num}_"
-        num += 2
-    end
-    if i % 2 == 1
-        print " #{num}$"
-        num += 2
-    end
-end
-puts ""
+# for i in 0..dato
+#     if i % 2 == 0
+#         print " #{num}_"
+#         num += 2
+#     elsif i % 2 == 1
+#         print " #{num}$"
+#         num += 2
+#     end
+# end
+# puts ""
 
+puts "ingrese un numero"
+n = gets.chomp.to_i
+
+    if n % 2 == 0
+        
+        v = 0
+        n /= 2
+        
+        for i in 1..n
+            print " #{v}_ "
+            v += 2
+        end
+    else
+        v = 1
+        n /= 2
+
+        for i in 1..n
+            print " #{v}$ "
+            v += 2
+        end
+    end
+puts " #{v} "
