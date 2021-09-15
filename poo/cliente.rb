d166ac2f5df5c8265eb030f92fe84ed07c6da275
oscramz <oscram@gmail.com>
2021-09-15 15:48:38 -0300

    Creación carpeta POO con ejercicios de la clase

---
+++ poo/cliente.rb
@@ -0,0 +1,26 @@
+class Cliente
+    # las clases siempre comienzan con mayúscula
+
+    # atributos /acciones / datos
+
+    # métodos / acciones / funciones
+    def saludar
+        puts "Hola soy un cliente"
+    end
+    
+    def imprimir_datos
+    end
+
+    def comprar_frutas
+    end
+
+    def actualizar_datos
+    end
+
+    def calcular_frecuencia_compras
+    end
+
+end
+
+cliente1 = Cliente.new
+cliente1.saludar
